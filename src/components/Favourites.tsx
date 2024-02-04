673d410be66d8fccb87d295b1f30fd79fea96ed8
Gabriele Di Grazia <gabrii539@gmail.com>
2024-02-04 10:36:56 +0100

    feat(React): created new Favourites page component

---
+++ src/components/Favourites.tsx
@@ -0,0 +1,27 @@
+import { Dispatch, SetStateAction } from 'react';
+import Card from './Card';
+
+function Favourites(
+  props: {
+    cards: {[key: string]: Card},
+    page: string,
+    setCards: Dispatch<SetStateAction<{[key: string]: Card}>>
+  }): JSX.Element {
+
+  console.log('Favourites rendered');
+
+  return (
+    <><h1>Favourites</h1>
+    {Object.keys(props.cards).map(id => {
+      if (props.cards[id].favourite) {
+        return (<Card 
+          title={props.cards[id].title} 
+          url={props.cards[id].url}
+          id={id}
+          page={props.page}
+        />)}
+    })}</>
+  );
+}
+
+export default Favourites;
