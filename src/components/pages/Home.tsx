7b7a9c5c9dfa7eff1ade84fa8445f72e64cac3a7
Gabriele Di Grazia <gabrii539@gmail.com>
2024-02-04 20:29:54 +0100

    feat(style): added section wrapper on cards

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Dispatch, SetStateAction } from 'react';
 import Form from '../Form';
 import Card from '../Card';
 
+
 function Home(
   props: {
     cards: CardCollection,
@@ -14,15 +15,17 @@ function Home(
     <>
       <h1>Home</h1>
       <Form setCards={props.setCards}/>
-      {Object.keys(props.cards).map(id => (
-          <Card 
-            title={props.cards[id].title}
-            url={props.cards[id].url}
-            id={id}
-            favourite={props.cards[id].favourite}
-            page={'home'}
-          />
-      ))}
+      <section id='cards'>
+        {Object.keys(props.cards).map(id => (
+            <Card 
+              title={props.cards[id].title}
+              url={props.cards[id].url}
+              id={id}
+              favourite={props.cards[id].favourite}
+              page={'home'}
+            />
+        ))}
+      </section>
     </>
   );
 }
