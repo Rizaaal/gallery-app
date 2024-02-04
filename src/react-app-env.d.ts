3bd1763334f626680fc4033fbba32f279ee2651c
Gabriele Di Grazia <gabrii539@gmail.com>
2024-02-04 10:26:17 +0100

    feat(Typescript): added TeaxtLabel interface

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+//props per il Button component custom che prende un figlio (una stringa)
+
+interface TextLabel {
+    children?: string
+    onClick?: any
+}
+
