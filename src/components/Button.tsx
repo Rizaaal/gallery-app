89a44860658437e59489b8975217cee8f7659d43
Gabriele Di Grazia <gabrii539@gmail.com>
2024-02-04 20:32:14 +0100

    feat(style): added StyledButton styled component on button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 
-// const StyledButton = styled.button`
+const StyledButton = styled.button`
+    border: none;
+    padding: .5em;
 
-// `
+    &:hover {
+        cursor: pointer;
+        background: #999;
+    }
+`
 
 function Button(props: ButtonProps): JSX.Element {
     return (
-        <button onClick={props.onClick}>
+        <StyledButton onClick={props.onClick}>
             {props.children ? props.children : "Click"}
-        </button>
+        </StyledButton>
     );
 }
 
