a70cde3798bcdede327d27aa7082305ab3d783d3
Gabriele Di Grazia <gabrii539@gmail.com>
2024-02-04 10:30:21 +0100

    feat(React): defined card state on App.js
    i also included a debug log in this commit.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  console.log('App rendered');
+  const [cards, setCards] = useState<{[key: string]: Card}>(
+    JSON.parse(localStorage.getItem("cards") || "{}")
+  );
   return (
     <h1>Galleria</h1>
   );
